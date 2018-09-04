97a254765e53b5750b7bfbae0fe2d7ace0ab0414
Eric <5615411+ewhill@users.noreply.github.com>
2018-09-04 14:00:59 -0400

    Removing debug flag from BYOHTTPSServer test.

--- test/testBYOHTTPSServer.js
+++ test/testBYOHTTPSServer.js
@@ -30,7 +30,7 @@ test("PeerBYOServerTest", (assert) => {
       'publicKey': "first.peer.pub",
       'privateKey': "first.peer.pem",
       'ringPublicKey': ".ring.pub",
-      'debug': true,
+      'debug': false,
       'discoveryRange': [26780, 26790],
       'startDiscovery': true,
       'requireConfirmation': true
