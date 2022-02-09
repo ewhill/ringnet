9c4d28e9e36b984b7b47832c504e014d7060be6d
Eric Hill <ew_hill@yahoo.com>
2022-02-09 16:30:03 -0500

    Fixing tests for helo and setupCipher message classes.

--- test/messages/testHeloMessage.js
+++ test/messages/testHeloMessage.js
@@ -4,23 +4,23 @@ const HeloMessage = require('../../lib/messages/helo');
 
 
 test("HeloMessage", (assert) => {
-  assert.throws(
-    () => new HeloMessage(), 
-    /required/,
-    "Attempting to create HeloMessage without params should throw.");
+  const emptyHeloMessage = new HeloMessage();
+
+  assert.equal(emptyHeloMessage.publicKey, undefined,
+    "Empty constructor value for publicKey should leave property unset.");
+  assert.equal(emptyHeloMessage.signature, undefined,
+    "Empty constructor value for signature should leave property unset.");
 
   const heloMessage = new HeloMessage({ publicKey: 'asd', signature: '123' });
 
   assert.equal(heloMessage.publicKey, 'asd',
     "Provided value for publicKey via constructor should set the publicKey.");
-
   assert.equal(heloMessage.signature, '123',
     "Provided value for signature via constructor should set the signature.");
 
   heloMessage.publicKey = 'a';
   assert.equal(heloMessage.publicKey, 'a',
     "Provided value for publicKey via setter should set the publicKey.");
-
   heloMessage.signature = 'b';
   assert.equal(heloMessage.signature, 'b',
     "Provided value for signature via setter should set the signature.");
--- test/messages/testSetupCipherMessage.js
+++ test/messages/testSetupCipherMessage.js
@@ -4,23 +4,23 @@ const SetupCipherMessage = require('../../lib/messages/setupCipher');
 
 
 test("SetupCipherMessage", (assert) => {
-  assert.throws(
-    () => new SetupCipherMessage(), 
-    /required/,
-    "Attempting to create SetupCipherMessage without params should throw.");
+  const emptySetupCipherMessage = new SetupCipherMessage();
+
+  assert.equal(emptySetupCipherMessage.key, undefined,
+    "Empty constructor value for key should leave property unset.");
+  assert.equal(emptySetupCipherMessage.iv, undefined,
+    "Empty constructor value for iv should leave property unset.");
 
   const cipherMessage = new SetupCipherMessage({ key: 'asd', iv: '123' });
 
   assert.equal(cipherMessage.key, 'asd',
     "Provided value for key via constructor should set the key.");
-
   assert.equal(cipherMessage.iv, '123',
     "Provided value for iv via constructor should set the iv.");
 
   cipherMessage.key = 'a';
   assert.equal(cipherMessage.key, 'a',
     "Provided value for key via setter should set the key.");
-
   cipherMessage.iv = 'b';
   assert.equal(cipherMessage.iv, 'b',
     "Provided value for iv via setter should set the iv.");
