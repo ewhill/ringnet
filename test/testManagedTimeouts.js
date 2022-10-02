85cdf2be67966d996d44fee8503a91a7954fa77e
Eric Hill <ew_hill@yahoo.com>
2022-10-02 14:44:43 -0400

    Making ManagedTimeouts naming (case) consistent.

--- test/testManagedTimeouts.js
+++ test/testManagedTimeouts.js
@@ -1,7 +1,7 @@
 "use strict";
 const test = require('tape');
 
-const ManagedTimeouts = require('../lib/managedTimeouts.js');
+const ManagedTimeouts = require('../lib/ManagedTimeouts.js');
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
